944d49826fa7d771206ed13550272847115353f6
MAGIC Reincarnated <139699919+MagicReincarnation@users.noreply.github.com>
2024-02-24 19:35:01 +0700

    Create hirutshujicode.js

---
+++ hirutshujicode.js
@@ -0,0 +1,49 @@
+const Truecode = true;
+var x456y7sz = 60;
+var e7k4d5c9r1p8tH = window.location.hostname;
+if (typeof firebase !== 'undefined' && typeof CryptoJS !== 'undefined') {
+    const llurijfirsdjdkensiCsjsjsodeDisjwjwjv = document.getElementById('hirutshuji_lisensi'); if (llurijfirsdjdkensiCsjsjsodeDisjwjwjv) {
+        const idmzkdewiwnxcryphxntedLisewsdndjdnsiCowjwjdendns = CryptoJS.AES.decrypt(llurijfirsdjdkensiCsjsjsodeDisjwjwjv.innerText.trim(), 'Hnssidfrfufftfsfhfufjffai').toString(CryptoJS.enc.Utf8);
+
+ var blswksogIjejwdMnetaasjsnsa = idmzkdewiwnxcryphxntedLisewsdndjdnsiCowjwjdendns;
+
+if (blswksogIjejwdMnetaasjsnsa) {
+            var bsjdcleoffgfItdUvsggetfessr = blswksogIjejwdMnetaasjsnsa;
+            const dajnddbfHfiderduddtsfhfud3jwiKffeeydd = `https://xhrji-617c0-default-rtdb.firebaseio.com/scriptbyHiru_lisensi/datauser_Hiru/${bsjdcleoffgfItdUvsggetfessr}.json`;
+            fetch(dajnddbfHfiderduddtsfhfud3jwiKffeeydd)
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.json();
+                    } else {
+                        throw new Error("Data not found");
+                    }
+                }).then(data => {
+                    var wksjhxnekcd = data.KodeHiru; 
+                    const wbwuianxewjanx = CryptoJS.AES.decrypt(wksjhxnekcd, 'Hnssidfrfufftfsfhfufjffai').toString(CryptoJS.enc.Utf8);
+                    
+if (data && wbwuianxewjanx === idmzkdewiwnxcryphxntedLisewsdndjdnsiCowjwjdendns) { 
+const Truecode = true;
+  codeNHiru();
+  svscadlidnafjadLsaicenfffse = true;
+                   
+} else {
+wrqardyfufuvjgwdyfibivjvtctdufdheeyf7guruefh();
+  }
+}).catch(error => {
+console.error("Error: ", error); 
+wrqardyfufuvjgwdyfibivjvtctdufdheeyf7guruefh();
+                });
+        } else {
+wrqardyfufuvjgwdyfibivjvtctdufdheeyf7guruefh();
+        }
+    } else { 
+wrqardyfufuvjgwdyfibivjvtctdufdheeyf7guruefh();
+    }
+} else {      wrqardyfufuvjgwdyfibivjvtctdufdheeyf7guruefh();
+}
+
+function wrqardyfufuvjgwdyfibivjvtctdufdheeyf7guruefh() {
+   const Truecode = false;
+    alert('Lisensi untuk kode ini kosong');
+  svscadlidnafjadLsaicenfffse = false;
+  }  
